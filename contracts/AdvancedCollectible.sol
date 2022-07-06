1e7f5f4b4318c932595455d5239c7284eb6666f3
ermz <eramirez4369@gmail.com>
2022-07-06 06:59:00 -0400

    Began working on AdvancedCollectible

---
+++ contracts/AdvancedCollectible.sol
@@ -0,0 +1,34 @@
+// An NFT Contract
+// Where the tokenURI can be on of 3 different dogs
+// Randomly selected
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract AdvancedCollectible is ERC721, VRFConsumerBase {
+    uint256 public tokenCounter;
+    bytes32 public keyhash;
+    uint256 public fee;
+    enum Breed{PUG, SHIBA_INU, ST_BERNARD}
+
+    constructor(address _vrfCoordinator, address _linkToken, bytes32 _keyhash, uint256 _fee) public
+    VRFConsumerBase(_vrfCoordinator, _linkToken)
+    ERC721("Dogie", "DOG") 
+    {
+        tokenCounter = 0;
+        keyhash = _keyhash;
+        fee = _fee;
+    }
+
+    function createCollectible(string memory tokenURI) public returns (bytes32) {
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
+        Breed breed = Breed(randomNumber % 3);
+    }
+
+}
